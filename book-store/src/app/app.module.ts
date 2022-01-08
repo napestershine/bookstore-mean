dcf3ae7738c9d574c078e44b984bce79d56e324f
Manu <napestershine@gmail.com>
2022-01-08 18:39:26 +0100

    begin auth module

--- book-store/src/app/app.module.ts
+++ book-store/src/app/app.module.ts
@@ -2,18 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   // componenets in this module
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   // Imports modules it depends on
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, AuthModule],
   providers: [],
   // Entry point for module
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
