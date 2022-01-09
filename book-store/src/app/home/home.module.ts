2ff21896b8bc0f624872fcb01f48594296a0c036
Manu <napestershine@gmail.com>
2022-01-09 17:10:17 +0100

    book module

--- book-store/src/app/home/home.module.ts
+++ book-store/src/app/home/home.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { BookModule } from '../book/book.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, BookModule],
   exports: [HomeComponent],
 })
 export class HomeModule {}
