6746aa3d565815b2194526be60682d6af3651cc6
Manu <napestershine@gmail.com>
2022-01-09 15:30:07 +0100

    home component with shared module

--- book-store/src/app/app.module.ts
+++ book-store/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   // componenets in this module
   declarations: [AppComponent],
   // Imports modules it depends on
-  imports: [BrowserModule, AuthModule],
+  imports: [BrowserModule, AuthModule, HomeModule],
   providers: [],
   // Entry point for module
   bootstrap: [AppComponent],
--- book-store/src/app/home/home.module.ts
+++ book-store/src/app/home/home.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [HomeComponent],
   imports: [CommonModule, SharedModule],
-  exports: [LoginComponent],
+  exports: [HomeComponent],
 })
-export class AuthModule {}
+export class HomeModule {}
